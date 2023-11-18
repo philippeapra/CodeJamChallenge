cae1b50ba1b466e16c72f1055a6c163fbecf8c02
philippeapra <philippeaprahamian6@gmail.com>
2023-11-18 11:36:29 -0500

    loads coordinates

--- connection.py
+++ connection.py
@@ -12,13 +12,16 @@ mqtt_user = "CodeJamUser"
 mqtt_password = "123CodeJam"
 trucks = {}
 loads = {}
-
+numOfTrucks=0
+numOfLoads=0
+load_coord=[]
 # Callback when connecting to the broker
 def on_connect(client, userdata, flags, rc):
-    with open("new_file.txt", "w") as file:
-        z=0
+    # with open("new_file.txt", "w") as file:
+    #     z=0
     print("Connected with result code "+str(rc))
     client.subscribe("CodeJam")
+
 
 # Callback when receiving a message
 def on_message(client, userdata, msg):
@@ -35,6 +38,9 @@ def on_message(client, userdata, msg):
                 event['equipType'],
                 event['nextTripLengthPreference']
             )
+            global numOfTrucks
+            numOfTrucks+=1
+
             trucks[event['truckId']] = truck
 
         elif event_type == 'Load':
@@ -49,15 +55,36 @@ def on_message(client, userdata, msg):
                 event['mileage']
             )
             loads[event['loadId']] = load
+            global numOfLoads
+            numOfLoads+=1
+            
+        elif event_type == 'End':
+           print("numOfLoads: "+str(numOfLoads))
+           print("numOfTrucks: " + str(numOfTrucks))
+           print("numOfUniqueLoads: "+str(len(loads)))
+           print("numOfUniqueTrucks: "+str(len(trucks)))
+           global load_coord
+           load_coord=[]
+           for key, value in loads.items():
+               load_coord.append(value.origin)
+           numOfLoads=0
+           numOfTrucks=0
+           print("Day ended")
+
+        elif event_type == 'Start':
+           print("Day started")
+           numOfLoads=0
+           numOfTrucks=0
+           
         
     except json.JSONDecodeError as e:
         print(f"Failed to parse JSON: {e}")
-    with open("new_file.txt", "w") as file:
-        #line = str(message_type)
+    # with open("new_file.txt", "w") as file:
+    #     #line = str(message_type)
         
-        #print(json.dumps(trucks, indent=4))
-        for truck in trucks:
-            file.write(str(trucks)+"\n"+str(loads)+"\n")
+    #     #print(json.dumps(trucks, indent=4))
+    #     for truck in trucks:
+    #         file.write(str(trucks)+"\n"+str(loads)+"\n")
 
 # Create MQTT client and set username and password
 client = mqtt.Client("ExceptionHandlers01")
@@ -72,5 +99,3 @@ client.connect(mqtt_broker, mqtt_port, 60)
 
 # Start the loop
 client.loop_forever()
-
-
